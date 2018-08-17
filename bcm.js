e330adbaa2c39aba24e7985722bc99141603eb46
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-18 00:23:27 +0300

    Create bcm.js

---
+++ bcm.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log('---------------');
+  console.log(' Bc Bot Is OnMusic')
+  console.log('---------------')
+  client.user.setStatus("Online")
+    client.user.setActivity(" Marine Games",{type: 'PLAYING'})
+});
+
+client.on('message',async message => {
+  if(message.content.startsWith("-bc")) {
+    let filter = m => m.author.id === message.author.id;
+    let thisMessage;
+    let thisFalse;
+    message.channel.send(':regional_indicator_b::regional_indicator_c:| **ارسل الرسالة الان**').then(msg => {
+
+    let awaitM = message.channel.awaitMessages(filter, {
+      max: 1,
+      time: 20000,
+      errors: ['time']
+    })
+    .then(collected => {
+      collected.first().delete();
+      thisMessage = collected.first().content;
+      msg.edit(':regional_indicator_b::regional_indicator_c:| **هل انت متأكد؟**');
+      let awaitY = message.channel.awaitMessages(response => response.content === 'نعم' || 'لا' && filter,{
+        max: 1,
+        time: 20000,
+        errors: ['time']
+      })
+      .then(collected => {
+        if(collected.first().content === 'لا') {
+          msg.delete();
+          message.delete();
+          thisFalse = false;
+        }
+        if(collected.first().content === 'نعم') {
+          if(thisFalse === false) return;
+        message.guild.members.forEach(member => {
+          msg.edit(':regional_indicator_b::regional_indicator_c:| **جاري الارسال**');
+          collected.first().delete();
+          member.send(`${thisMessage}\n\n${member} ,\nتم الارسال من : ${message.guild.name}\n تم الارسال بواسطة : ${message.author.tag}`);
+        });
+        }
+      });
+    });
+    });
+  }
+});
+client.login(process.env.BOT_TOKEN);  //لا تحط التوكن حقك هنا
