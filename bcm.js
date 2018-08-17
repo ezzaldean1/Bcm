7ec536c9c0aa64c4f09cb08692b7257e79628e08
ezzaldean1 <42187031+ezzaldean1@users.noreply.github.com>
2018-08-18 00:36:01 +0300

    Update bcm.js

--- bcm.js
+++ bcm.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 });
 
 var prefix = "-"
-lient.on('message', message => {
+client.on('message', message => {
    if(!message.channel.guild) return;
 if(message.content.startsWith(prefix + 'bc')) {
 if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
